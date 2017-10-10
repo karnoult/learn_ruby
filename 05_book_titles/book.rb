ff1fd753b1065f76e82aa4031b9e9520051eeea5
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-10-10 18:14:06 +0200

    Fix exercize 05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,26 @@
 class Book
-# write your code here
+	attr_accessor :title
+
+	def title=(new_title)
+		@title = titleize(new_title)
+	end
+
+	def titleize_firstletter(word)
+		return word.chars[0].upcase + word.chars[1..word.size].join
+	end
+
+	def titleize(welcome)
+		little_words = ["the", "and", "in", "of", "a", "an"]
+		final_word = ""
+
+		welcome.split(" ").each do |word|
+			if final_word.size == 0 || !little_words.include?(word)
+				final_word += titleize_firstletter(word) + " "
+			else
+				final_word += word + " "
+			end
+		end
+		return final_word.rstrip
+	end
+
 end
