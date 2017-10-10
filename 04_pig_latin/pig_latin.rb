1d94f05dd1f831ecbc25277162a47ba648f3b012
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-10-10 18:05:03 +0200

    Fix exercize 04

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,50 @@
-#write your code here
+def is_vowel(letter)
+	return ["a", "e", "i", "o", "u"].include? letter
+end
+
+def translate_word(word)
+
+	single_phoneme = ["qu"]
+	start_with_phoneme = ""
+
+	my_word = ""
+	
+	single_phoneme.each do |phoneme|
+		
+		start_with_phoneme = phoneme if word.start_with?(phoneme)
+		if(start_with_phoneme == "" && !is_vowel(word[0]))
+			start_with_phoneme = word[0] + phoneme if word[1..word.size].start_with?(phoneme)
+		end
+		
+	end
+
+	if start_with_phoneme != ""
+		my_word = word[start_with_phoneme.size..word.size] + start_with_phoneme + "ay"
+	elsif is_vowel(word[0])
+		my_word = word + "ay"
+	else
+		index = 0
+		move_letters = ""
+		while !is_vowel(word[index]) do
+			move_letters << word[index]
+			index += 1
+		end
+		my_word = word[index..word.size] + move_letters + "ay"
+	end
+
+	return my_word
+
+end
+
+
+def translate(phrase)
+
+	new_phrase = ""
+
+	phrase.split(" ").each do |word|
+		new_phrase << translate_word(word) + " "
+	end
+
+	return new_phrase.rstrip
+
+end
