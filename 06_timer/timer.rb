62ed5d99f231e88c898ac9efe619c2965e475074
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-10-10 18:30:09 +0200

    Fix exercize 06

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,17 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+  	@seconds = 0
+  end
+
+  def time_string
+
+  	hours = @seconds / 3600
+  	minutes = (@seconds % 3600) / 60
+  	seconds = @seconds % 60
+
+  	return ("%02d" % hours) + ":" + ("%02d" % minutes) + ":" + ("%02d" % seconds)
+
+  end
 end
