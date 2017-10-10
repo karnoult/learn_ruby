9ee83dfe3b0edca8ceed0c40894e63675f3ca557
Kévin Arnoult <kevin.arnoult@gmail.com>
2017-10-10 17:24:58 +0200

    Fix exercize 03

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,39 @@
-#write your code here
+def echo(welcome)
+	welcome
+end
+
+def shout(welcome)
+	welcome.upcase
+end
+
+def repeat(welcome, times = 2)
+	((welcome + " ") * times).rstrip
+end
+
+def start_of_word(welcome, number)
+	welcome[0..number-1]
+end
+
+def first_word(welcome)
+	welcome.split(" ")[0]
+end
+
+def titleize_firstletter(word)
+	return word.chars[0].upcase + word.chars[1..word.size].join
+end
+
+def titleize(welcome)
+	
+	little_words = ["the", "and", "over"]
+	final_word = ""
+
+	welcome.split(" ").each do |word|
+		if final_word.size == 0 || !little_words.include?(word)
+			final_word += titleize_firstletter(word) + " "
+		else
+			final_word += word + " "
+		end
+	end
+	return final_word.rstrip
+
+end
